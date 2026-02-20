221ed0f5ec1451eb9cfbd33260f130ba961d8457
JOSUE <joshvm1235@gmail.com>
2026-02-20 02:29:15 -0600

    mejora del navbar

--- frontend/src/app/layout/navbar/navbar.component.ts
+++ frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,8 +1,19 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  menuOpen = signal(false);
+
+  toggleMenu() {
+    this.menuOpen.update(value => !value);
+  }
+
 }
