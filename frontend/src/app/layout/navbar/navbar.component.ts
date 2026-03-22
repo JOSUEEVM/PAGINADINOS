fb1ed2ee595ecbe9a18cbac68e353f649a348922
JOSUEEVM <joshvm1235@gmail.com>
2026-03-22 17:23:29 -0600

    navbar dinamico

--- frontend/src/app/layout/navbar/navbar.component.ts
+++ frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,5 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+  Component,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  signal,
+  Inject
+} from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,12 +16,72 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit, OnDestroy {
   menuOpen = signal(false);
+  navbarHidden = signal(false);
+
+  private hideTimer: ReturnType<typeof setTimeout> | null = null;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit(): void {
+    this.showNavbar();
+  }
+
+  ngOnDestroy(): void {
+    if (this.hideTimer) {
+      clearTimeout(this.hideTimer);
+    }
+    this.document.body.classList.remove('navbar-hidden');
+  }
 
   toggleMenu() {
     this.menuOpen.update(value => !value);
+    this.showNavbar();
   }
 
+  showNavbar() {
+    this.navbarHidden.set(false);
+    this.document.body.classList.remove('navbar-hidden');
+    this.startHideTimer();
+  }
+
+  private hideNavbar() {
+    this.navbarHidden.set(true);
+    this.document.body.classList.add('navbar-hidden');
+  }
+
+  private startHideTimer() {
+    if (this.hideTimer) {
+      clearTimeout(this.hideTimer);
+    }
+
+    this.hideTimer = setTimeout(() => {
+      if (window.scrollY > 0 && !this.menuOpen()) {
+        this.hideNavbar();
+      }
+    }, 2000);
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    if (window.scrollY === 0) {
+      this.navbarHidden.set(false);
+      this.document.body.classList.remove('navbar-hidden');
+
+      if (this.hideTimer) {
+        clearTimeout(this.hideTimer);
+      }
+      return;
+    }
+
+    this.showNavbar();
+  }
+
+  @HostListener('window:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (event.clientY <= 80) {
+      this.showNavbar();
+    }
+  }
 }
